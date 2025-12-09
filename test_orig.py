322a8c08b4c6f2ef4007efd87c93d02a836e4666
anna <afgnkad@gmail.com>
2025-12-09 16:50:47 +0300

    add sound

---
+++ test_orig.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
+
+from window_orig import MainWindow
+
+app = QApplication(sys.argv)
+
+#w = QMainWindow()
+w = MainWindow()
+
+w.setWindowTitle("Плеер")
+w.show()
+
+app.exec()
