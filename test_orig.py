c351bb118a17d14ac566f6892c4ab187a30c9e92
anna <afgnkad@gmail.com>
2025-12-21 09:44:59 +0300

    added converter not tested

--- test_orig.py
+++ test_orig.py
@@ -1,13 +1,9 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
-
 from window_orig import MainWindow
 
 app = QApplication(sys.argv)
-
-#w = QMainWindow()
 w = MainWindow()
-
 w.setWindowTitle("Плеер")
 w.show()
 
